7208fbe796c5fd9add97907ac4cb2f960bbe2754
Lukas Da Silva De Souza <Lukas_Souza@bracell.com>
2025-05-08 08:15:06 -0300

    Organização estrutura e correção path para deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const path = require('path');
 const db = require('./db');
 
 const app = express();
